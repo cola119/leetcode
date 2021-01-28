d0d22d5a0387732dfd925a56fa0fb01aa9d5b6e5
cola119 <k.cola119@ezweb.ne.jp>
2021-01-29 00:06:15 +0900

    stack

---
+++ 112-path-sum/third/main.cpp
@@ -0,0 +1,76 @@
+#include <cassert>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        if (root == nullptr)
+            return false;
+        stack<pair<TreeNode *, int>> s;
+        s.push({root, targetSum});
+        while (!s.empty())
+        {
+            pair<TreeNode *, int> p = s.top();
+            s.pop();
+            int score = p.second - p.first->val;
+            if (score == 0 && isLeaf(p.first))
+                return true;
+            if (p.first->right != nullptr)
+                s.push({p.first->right, score});
+            if (p.first->left != nullptr)
+                s.push({p.first->left, score});
+        }
+
+        return false;
+    }
+
+    bool isLeaf(TreeNode *node)
+    {
+        return node->right == nullptr && node->left == nullptr;
+    }
+};
+
+int main()
+{
+    Solution *solver = new Solution();
+
+    TreeNode *root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2)), new TreeNode()), new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(), new TreeNode(1))));
+    assert(solver->hasPathSum(root, 22) == true);
+
+    root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    assert(solver->hasPathSum(root, 5) == false);
+
+    root = new TreeNode();
+    assert(solver->hasPathSum(root, 1) == false);
+
+    root = new TreeNode(-2, new TreeNode(), new TreeNode(-3));
+    assert(solver->hasPathSum(root, -5) == true);
+
+    return 0;
+}
